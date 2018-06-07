0fb63e22a4364e74cc0e97c71c456f5c22d143a1
Jatin Gupta <jatin.gupta@nagarro.com>
2018-06-07 13:19:54 +0530

    first commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var linechart2722B7D3FF8B443E382E3AD83079C217E_DEBUG = {
+        name: 'linechart2722B7D3FF8B443E382E3AD83079C217E_DEBUG',
+        displayName: 'linechart2',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.11.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.linechart2722B7D3FF8B443E382E3AD83079C217E.Visual(options),
+        custom: true
+    };
+}
---
+++ src/visual.ts
@@ -0,0 +1,129 @@
+/*
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+module powerbi.extensibility.visual {
+    "use strict";
+    export class Visual implements IVisual {
+        private target: HTMLElement;
+        private updateCount: number;
+        private settings: VisualSettings;
+        private textNode: Text;
+        private temp_max = [279.946, 282.597, 280.15];
+        private temp_min = [279.946, 282.597, 278.15];
+        private allDates = [1485717216, 1485745061, 1485768552];
+
+        constructor(options: VisualConstructorOptions) {
+            console.log('Visual constructor', options);
+            this.target = options.element;
+            this.updateCount = 0;
+            if (typeof document !== "undefined") {
+                const new_p: HTMLElement = document.createElement("div");
+                const can: HTMLElement = document.createElement("canvas");
+                can.setAttribute("id", "canvasId");
+                new_p.appendChild(can);
+
+                new_p.appendChild(document.createTextNode("Update count:"));
+                const new_em: HTMLElement = document.createElement("em");
+                this.textNode = document.createTextNode(this.updateCount.toString());
+                new_em.appendChild(this.textNode);
+                new_p.appendChild(new_em);
+                this.target.appendChild(new_p);
+            }
+        }
+
+        public update(options: VisualUpdateOptions) {
+            this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+            console.log(options.dataViews[0].table.rows);
+            const data = options.dataViews[0].table.rows;
+            let country = [];
+            let value = [];
+            data.forEach(d=>{
+                country.push(d[0]);
+                value.push(d[1]);
+            });
+            console.log('--------------')
+            console.log(country);
+            console.log(value);
+
+
+
+            let weatherDates = [];
+            this.allDates.forEach((eachDate) => {
+                let jsDate = new Date(eachDate * 1000);
+                weatherDates.push(jsDate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}));
+            });
+            let d = (<any>window).Chart;
+            //let ctx = document.getElementById("canvasId");
+
+            let chart =  new d('canvasId' , {
+                type: 'line',
+                data: {
+                    labels: country,
+                    datasets: [
+                        {
+                            data: value,
+                            borderColor: '#3cba9f',
+                            fill: false
+                        }
+                    ]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true
+                        }],
+                        yAxes: [{
+                            display: true
+                        }]
+                    }
+                }
+            });
+
+
+
+            console.log('Visual update', options);
+            if (typeof this.textNode !== "undefined") {
+                this.textNode.textContent = (this.updateCount++).toString();
+            }
+        }
+
+        private static parseSettings(dataView: DataView): VisualSettings {
+            return VisualSettings.parse(dataView) as VisualSettings;
+        }
+
+        /** 
+         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
+         * objects and properties you want to expose to the users in the property pane.
+         * 
+         */
+        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+            return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        }
+    }
+}
